e02cb0e83eea5e2c419125f0125b789568075986
jialutu <jialutu@gmail.com>
2016-02-18 22:04:04 +0000

    Update Yahoofinance.py

--- Yahoofinance.py
+++ Yahoofinance.py
@@ -9,6 +9,9 @@ from matplotlib import style
 
 style.use('ggplot')
 
+MA1=10
+MA2=30
+
 #print(plt.style.available)
 #print(plt.__file__)
 
@@ -19,14 +22,26 @@ def bytespdate2num(fmt,encoding='utf-8'):
         return str_converter(s)
     return bytes_converter
 
+def moving_average(values,window):
+    weights=np.repeat(1.0, window)/window
+    smas=np.convolve(values,weights,'valid')
+    return smas
+
+def high_minus_low(highs,lows):
+    return highs-lows
+
 def graph_data(stock):
     fig=plt.figure()
-    ax1=plt.subplot2grid((1,1),(0,0))
-    plt.ylabel('price')
-    plt.xlabel('date')
+    ax1=plt.subplot2grid((6,1),(0,0),rowspan=1,colspan=1)
+    plt.ylabel('H-L')
+    ax2=plt.subplot2grid((6,1),(1,0),rowspan=4,colspan=1,sharex=ax1)
+    plt.ylabel('Price')
+    ax3=plt.subplot2grid((6,1),(5,0),rowspan=1,colspan=1,sharex=ax1)
+    plt.ylabel('MAvgs')
+#    plt.xlabel('date')
 
     print('Currently pulling:',stock)
-    url = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=3m/csv'
+    url = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=6m/csv'
     source_code=urllib.request.urlopen(url).read().decode()
     stock_data=[]
     split_source=source_code.split('\n')
@@ -43,6 +58,10 @@ def graph_data(stock):
 #    date_conv = np.vectorize(dt.datetime.fromtimestamp)
 #    date = date_conv(date)
 
+    ma1=moving_average(closep,MA1)
+    ma2=moving_average(closep,MA2)
+    start=len(date[MA2-1:])
+
     x=0
     y=len(date)
 
@@ -51,6 +70,10 @@ def graph_data(stock):
         append_line = date[x], closep[x],highp[x],lowp[x],openp[x],volume[x]
         new_list.append(append_line)
         x+=1
+    h_l=list(map(high_minus_low,highp,lowp))
+    ax1.plot_date(date[-start:],h_l[-start:],'-')
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    ax1.yaxis.set_major_locator(mticker.MaxNLocator(nbins=5,prune='lower'))
 
     #ax1.fill_between(date, closep, 146, alpha=0.5, edgecolor='r')
 #    ax1.plot_date(date,closep,'-')
@@ -59,8 +82,8 @@ def graph_data(stock):
 #    ax1.axhline(152, color='r')
 #    ax1.axhline(155, color='g')
 
-    candlestick_ohlc(ax1, new_list, width=.6, colorup='#ff1717', colordown='#41ad49')
-    ax1.grid(False)
+    candlestick_ohlc(ax2, new_list[-start:], width=.6, colorup='#ff1717', colordown='#41ad49')
+    ax2.grid(False)
 #    ax1.yaxis.label.set_color('m')
 #    ax1.xaxis.label.set_color('c')
 #    ax1.set_yticks([146,155,164])
@@ -72,19 +95,26 @@ def graph_data(stock):
 #    ax1.spines['bottom'].set_linewidth(5)
 
     plt.title(stock)
-    for label in ax1.xaxis.get_ticklabels():
+    for label in ax2.xaxis.get_ticklabels():
         label.set_rotation(45)
 
-    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    ax1.annotate('Zuma fired finance minister!', (date[17],openp[17]), xytext=(0.6,0.9), textcoords='axes fraction',
-                 arrowprops=dict(facecolor="#585858"))
-    bbox_props=dict(boxstyle='larrow, pad=0.3', fc='#f2f1f1', ec='k', lw=2)
-    ax1.annotate(str(closep[-1]), (date[-1],closep[-1]), xytext=(date[-1]+2.5,closep[-1]), bbox=bbox_props)
+    #ax2.annotate('Zuma fired finance minister!', (date[15],openp[15]), xytext=(0.6,0.9), textcoords='axes fraction',
+    #             arrowprops=dict(facecolor="#585858"))
+    bbox_props=dict(boxstyle='larrow, pad=0.3', fc='#c5cbdf', ec='k', lw=2)
+    ax2.annotate(str(closep[-1]), (date[-1],closep[-1]), xytext=(date[-1]+2.5,closep[-1]), bbox=bbox_props)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    ax2.yaxis.set_major_locator(mticker.MaxNLocator(nbins=5,prune='upper'))
 
-    plt.ylabel('Price')
+    ax3.plot(date[-start:],ma1[-start:], linewidth=1)
+    ax3.plot(date[-start:],ma2[-start:], linewidth=1)
+    ax3.fill_between(date[-start:], ma2[-start:],ma1[-start:],where=(ma2[-start:]>=ma1[-start:]),facecolor='r',edgecolor='r',alpha=0.5)
+    ax3.fill_between(date[-start:], ma2[-start:],ma1[-start:],where=(ma2[-start:]<=ma1[-start:]),facecolor='g',edgecolor='g',alpha=0.5)
 
-    plt.subplots_adjust(left=.09,bottom=.16,right=.94,top=.95,wspace=.2,hspace=.2)
+    ax3.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax3.yaxis.set_major_locator(mticker.MaxNLocator(nbins=5,prune='upper'))
+
+    plt.subplots_adjust(left=.09,bottom=.16,right=.94,top=.95,wspace=.2,hspace=.02)
     plt.show()
 
 stock = input('Stock to plot: ')
