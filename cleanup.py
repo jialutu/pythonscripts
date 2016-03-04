60bf8efc21aae2137fac0824548755f39c6552c9
jialutu <jialutu@gmail.com>
2016-03-04 21:55:52 +0000

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,10 +1,11 @@
 import os, time, sys
 
-path = r"C:\temp"
-now = time.time()
+def cleanup(path):
+    f = os.listdir(path)
+    now = time.time()
 
-for f in os.listdir(path):
-    f = os.path.join(path, f)
-    if os.stat(f).st_mtime < now - 60 * 86400:
-        if os.path.isfile(f):
-            os.remove(f)
+    for a in f:
+        a=os.path.join(path, a)
+        if os.stat(a).st_mtime < now - 60 * 86400:
+            if os.path.isfile(a):
+                os.remove(a)
