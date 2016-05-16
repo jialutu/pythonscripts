188f07f0d753c5c8eda730b9f962c9384be53d5c
jialutu <jialutu@gmail.com>
2016-05-16 16:23:33 +0100

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,6 +1,9 @@
 import pandas as pd
-import Quandl
+from Quandl import Quandl
 import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = Quandl.get('WIKI/GOOGL')
 
@@ -11,8 +14,6 @@ df['PCT_Change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] *100.0
 
 df=df[['Adj. Close','HL_PCT','PCT_Change','Adj. Volume']]
 
-print(df.head())
-
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
@@ -20,7 +21,19 @@ forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label']=df[forecast_col].shift(-forecast_out)
 
-print(df.head())
+X = np.array(df.drop(['label'],1))
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+x = preprocessing.scale(X)
 
 df.dropna(inplace=True)
-print(df.tail())
+y = np.array(df['label'])
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
