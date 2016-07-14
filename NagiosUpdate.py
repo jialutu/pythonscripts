3107a2e07ece49562e32de669d3a0229f1c2412c
jialutu <jialutu@gmail.com>
2016-07-14 14:10:30 +0100

    Update NagiosUpdate.py

--- NagiosUpdate.py
+++ NagiosUpdate.py
@@ -1,23 +1,27 @@
 import os
 import fnmatch
+import re
 
-a = "C:/Users/jtu/Documents/pb-nagios/files/pb_objects/services/UAT/gresham"
+a = "C:/Users/jtu/Documents/pb-nagios/files/pb_objects/services/UAT/centurylink"
 
-folders = os.listdir(a)
+def NagiosUpdate(a):
+    folders = os.listdir(a)
 
-for folder in folders:
-    b = a+'/'+folder
-    files = os.listdir(b)
-    for file in files:
-        if fnmatch.fnmatch(file, 'dsk_*.cfg'):
-            c = b+'/'+file
+    for folder in folders:
+        b = a+'/'+folder
+        files = os.listdir(b)
+        for file in files:
+            if fnmatch.fnmatch(file, 'dsk_*.cfg'):
+                c = b+'/'+file
 
-            f = open(c,mode='r+')
-            filedata = f.read()
-            f.close()
+                f = open(c,mode='r+')
+                filedata = f.read()
+                f.close()
 
-            newdata = filedata.replace("pb_247_freq3-service","pb_247_freq15-service")
+                newdata = re.sub("pb_247_freq\d-service","pb_247_freq15-service",filedata)
 
-            f = open(c,'w')
-            f.write(newdata)
-            f.close()
+                f = open(c,'w')
+                f.write(newdata)
+                f.close()
+
+NagiosUpdate(a)
